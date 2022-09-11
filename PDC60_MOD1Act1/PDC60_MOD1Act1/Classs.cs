e5455f228cc568b56c4710902ca96ea5a6e70718
Aaron Chris Deloso <deloso.aaronchris@auf.edu.ph>
2022-09-11 22:52:52 +0800

    Add project files.

---
+++ PDC60_MOD1Act1/PDC60_MOD1Act1/Classs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PDC60_MOD1Act1
+{
+    public class Classs
+    {
+
+        public static double NormalFontSize = 25;
+
+        public static readonly Color ButtonColor = Color.BurlyWood;
+        public static readonly Color TextColor = Color.White;
+        public static readonly Color CenterColor = Color.White;            
+        public static readonly Color ButtonTextColor = Color.White;
+
+    }
+}
